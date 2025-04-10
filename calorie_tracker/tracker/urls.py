2192c4496060c6259b01f43a9b9967c3a424f2cb
Radhika Godambe <155623245+radhikaaa25@users.noreply.github.com>
2025-04-10 13:59:00 +0530

    Delete calorie_tracker directory

--- calorie_tracker/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('add_food/', views.add_food, name='add_food'),
-    path('set_goal/', views.set_goal, name='set_goal'),
-]
